25a3134cfdc82447742f1e2410f31bd817ccb8a9
Shefali <shefali4@illinois.edu>
2021-01-08 01:14:57 -0600

    Added default image, pronunciation, example, image, error handling, etc.

--- popup.js
+++ popup.js
@@ -14,10 +14,28 @@ document.addEventListener('DOMContentLoaded', function showWordDef() {
         return response.json();
     })
     .then((data) => {
+
+        //Getting example, pronounciation, word, definition, image, etc.
+        if (!(data.pronunciation == `${word}`)) {
+            document.getElementById("pronounce").innerText = data.pronunciation;
+        } 
+        
+        document.getElementById("exampleArea").innerText = data.definitions[0].example;
+        document.getElementById("typeArea").innerText = data.definitions[0].type;
         document.getElementById("definitionArea").innerText = data.definitions[0].definition;
+        document.getElementById("imageArea").src = data.definitions[0].image_url;
+        document.getElementById("imageArea").setAttribute("src", data.definitions[0].image_url);
+        
+        if (data.definitions[0].image_url == null) {
+            document.getElementById("imageArea").setAttribute("src", "Default.png");
+        }
     })
+
+    //Error handling
     .catch(error => {
         document.getElementById("definitionArea").innerText = "No definition found. Please use search feature!";
+        document.getElementById("exampleArea").innerText = "No example found";
+        document.getElementById("imageArea").setAttribute("src", "Default.png");
       })
 });
 
